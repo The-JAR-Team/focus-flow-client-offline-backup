d1d04bb881b965680a92faf47f38ae1924dd467c
renan <renanbazinin2@gmail.com>
2025-03-19 20:43:13 +0200

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import { FaceMeshProvider } from './components/FaceMeshContext'; // import your provider
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FaceMeshProvider>
+    <BrowserRouter basename="/focus-flow-client">
+
       <App />
-    </FaceMeshProvider>
+      </BrowserRouter>
+
   </React.StrictMode>
 );
