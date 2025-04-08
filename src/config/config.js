0ad5047f6904f11a375c4ee89e68770f37e7f1d3
Jonathan Shaya <jonathansh2@mta.ac.il>
2025-04-08 20:51:44 +0300

    added local ips

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 export const config = {
-    //baseURL: 'http://localhost:3001', // Change this according to your backend URL
+    // baseURL: 'http://127.0.0.1:5000', // Change this according to your backend URL
     baseURL: 'https://focus-flow-236589840712.me-west1.run.app',
 };
 
 export const BASE_URL = 'https://focus-flow-236589840712.me-west1.run.app';
+// export const BASE_URL = 'http://127.0.0.1:5000';
