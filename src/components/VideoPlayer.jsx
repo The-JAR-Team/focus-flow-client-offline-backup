9f918ebf95b181848903088669aae3c416529a24
renan <renanbazinin2@gmail.com>
2025-04-10 01:23:03 +0300

    this is good version but now can start interval from 500ms

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -461,7 +461,7 @@ const handleNoClientPauseToggle = () => {
         <label>Send Interval: {sendIntervalSeconds}s</label>
         <input
           type="range"
-          min="2"
+          min="0.5"
           max="10"
           value={sendIntervalSeconds}
           onChange={(e) => handleIntervalChange(e.target.value)}
