0d776bb9b5f122d62c419248746553143c807767
renan <renanbazinin2@gmail.com>
2025-04-10 01:08:18 +0300

    Revert "false debuggin"
    This reverts commit 6366343c6e92e6dc20870d3c20a6d910cc414ec5.

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    //baseURL: 'http://localhost:5000', // Change this according to your backend URL
+    //baseURL: 'http://localhost:3001', // Change this according to your backend URL
     baseURL: 'https://focus-flow-236589840712.me-west1.run.app',
 };
 
